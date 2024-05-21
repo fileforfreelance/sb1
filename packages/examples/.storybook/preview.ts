18e85d272982a6d0181c63a4df850dd23ee4412c
fileforfreelance <74954312+fileforfreelance@users.noreply.github.com>
2024-05-21 18:42:16 +0700

    1

---
+++ packages/examples/.storybook/preview.ts
@@ -0,0 +1,14 @@
+// Replace your-framework with the framework you are using (e.g., react, vue3)
+import { Preview } from '@storybook/addon-designs';
+
+import { themes } from '@storybook/theming';
+
+const preview: Preview = {
+  parameters: {
+    docs: {
+      theme: themes.dark,
+    },
+  },
+};
+
+export default preview;
