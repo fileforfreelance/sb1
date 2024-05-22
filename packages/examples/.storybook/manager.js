018b8ccd4da9745c31a433d226594b99b43b1c75
fileforfreelance <74954312+fileforfreelance@users.noreply.github.com>
2024-05-22 13:28:14 +0700

    1

--- packages/examples/.storybook/manager.js
+++ packages/examples/.storybook/manager.js
@@ -4,12 +4,3 @@ import yourTheme from './YourTheme';
 addons.setConfig({
   theme: yourTheme,
 });
-
-// manager.js
-
-import favicon from './assets/favicon1.png';
-
-const link = document.createElement('link');
-link.setAttribute('rel', 'shortcut icon');
-link.setAttribute('href', favicon);
-document.head.appendChild(link);
