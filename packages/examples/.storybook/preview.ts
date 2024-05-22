30784a3218df85045c3b5a2f1063527d7c5d1a3b
fileforfreelance <74954312+fileforfreelance@users.noreply.github.com>
2024-05-22 15:01:07 +0700

    1

--- packages/examples/.storybook/preview.ts
+++ packages/examples/.storybook/preview.ts
@@ -1,5 +1,5 @@
 // Replace your-framework with the framework you are using (e.g., react, vue3)
-import { Preview } from '@storybook/types';
+import { Preview } from '@storybook/react-webpack5';
 
 import { themes } from '@storybook/theming';
 
